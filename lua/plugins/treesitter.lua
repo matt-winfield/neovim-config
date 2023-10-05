e85676d750179060ac34161874e03b6a024d1145
Matthew Winfield <matthew.thomas.winfield@gmail.com>
2023-10-05 20:01:39 +0100

    Add keybinds for treesitter increment selection to <cr> and <tab>

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = {
+      ensure_installed = {
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+        "prisma",
+        "rust",
+        "dockerfile",
+        "gitignore",
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          -- C-space doesn't work in windows terminal, so use <cr> and <tab> instead
+          init_selection = "<cr>",
+          node_incremental = "<cr>",
+          scope_incremental = "<tab>",
+          node_decremental = "<s-tab>",
+        },
+      },
+    },
+  },
+}
