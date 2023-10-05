34b917c37a233d8c34646c1f10e07d0f28370b1a
Matthew Winfield <Matthew_Winfield@Derivco.co.uk>
2023-10-05 13:08:47 +0100

    Add harpoon

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,26 @@
+local wk = require("which-key")
+
+wk.register({
+  ["<leader>h"] = {
+    name = "+harpoon",
+  },
+})
+
+return {
+  "ThePrimeagen/harpoon",
+  opts = {
+    save_on_toggle = true,
+  },
+  keys = {
+    { "<leader>ha", "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = "Add file" },
+    { "<leader>hr", "<cmd>lua require('harpoon.mark').rm_file()<cr>", desc = "Remove file" },
+    { "<leader>hh", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Harpoon menu" },
+    { "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<cr>", desc = "Next file" },
+    { "<leader>hp", "<cmd>lua require('harpoon.ui').nav_prev()<cr>", desc = "Previous file" },
+    { "<leader>h1", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", desc = "File 1" },
+    { "<leader>h2", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", desc = "File 2" },
+    { "<leader>h3", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>", desc = "File 3" },
+    { "<leader>h4", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>", desc = "File 4" },
+    { "<leader>h5", "<cmd>lua require('harpoon.ui').nav_file(5)<cr>", desc = "File 5" },
+  },
+}
