15f8bbc91fe7bd22c4e4dddb83270e95cd7929b9
Matthew Winfield <Matthew_Winfield@Derivco.co.uk>
2023-10-06 09:38:25 +0100

    Increase conform timeout

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -22,7 +22,7 @@ return {
         ["handlebars"] = { { "prettierd", "prettier" } },
       },
       format_on_save = {
-        timeout_ms = 500,
+        timeout_ms = 1500,
         lsp_fallback = true,
       },
     },
