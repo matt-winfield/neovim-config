2570667cdb5d78a960942b7be982e7495a63841f
Matthew Winfield <Matthew_Winfield@Derivco.co.uk>
2023-10-11 14:37:55 +0100

    Improve line numbers contrast

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -8,7 +8,10 @@ return {
         floats = "transparent",
       },
       on_highlights = function(hl, c)
-        hl.NeoTreeMessage = hl.NeoTreeDimText
+        hl.NeoTreeMessage = hl.NeoTreeDimText -- (1 Hidden Item message, for example. By default not set so has poor contrast)
+
+        hl.LineNr = { fg = "#737aa2" } -- Set the gutter line numbers to have better contrast than the default
+        hl.CursorLineNr = { fg = "#c2c5d6" }
       end,
     },
   },
