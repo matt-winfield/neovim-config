65b48f6da4b04f349b4d03df2627b99c7c378005
Matthew Winfield <Matthew_Winfield@Derivco.co.uk>
2023-10-11 08:32:28 +0100

    Change neoscroll to mini.animate

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.util.project" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "plugins" },
--- lua/plugins/overrides.lua
+++ lua/plugins/overrides.lua
@@ -90,27 +90,6 @@ return {
     },
   },
   {
-    "karb94/neoscroll.nvim",
-    lazy = false,
-    config = function()
-      require("neoscroll").setup()
-
-      -- Override mappings to set timing
-      local t = {}
-      t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "100" } }
-      t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "100" } }
-      t["<C-b>"] = { "scroll", { "-vim.api.nvim_win_get_height(0)", "true", "200" } }
-      t["<C-f>"] = { "scroll", { "vim.api.nvim_win_get_height(0)", "true", "200" } }
-      t["<C-y>"] = { "scroll", { "-0.10", "false", "100" } }
-      t["<C-e>"] = { "scroll", { "0.10", "false", "100" } }
-      t["zt"] = { "zt", { "100" } }
-      t["zz"] = { "zz", { "100" } }
-      t["zb"] = { "zb", { "100" } }
-
-      require("neoscroll.config").set_mappings(t)
-    end,
-  },
-  {
     "github/copilot.vim",
   },
   {
