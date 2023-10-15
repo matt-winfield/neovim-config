0b18e535db327f4b30e7b2932f630116ef47ac0d
Matthew Winfield <matthew.thomas.winfield@gmail.com>
2023-10-15 17:58:04 +0100

    Re-enable flash keybinds

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,28 +1,12 @@
 return {
   {
-    -- Disable flash keybindings, keybindings conflict with mini.surround
-    -- The enhanced f, F, t and T behaviour will still work
-    -- Otherwise I just use / and ? for search
-    "folke/flash.nvim",
-    keys = {
-      { "s", mode = { "n", "x", "o" }, false, desc = "Flash" },
-      { "S", mode = { "n", "o", "x" }, false, desc = "Flash Treesitter" },
-      { "r", mode = "o", false, desc = "Remote Flash" },
-      { "R", mode = { "o", "x" }, false, desc = "Treesitter Search" },
-      { "<c-s>", mode = { "c" }, false, desc = "Toggle Flash Search" },
-    },
-  },
-  {
     "echasnovski/mini.surround",
     opts = {
       mappings = {
         add = "gs",
         delete = "ds",
-        find = "sf",
-        find_left = "sF",
-        highlight = "sh",
         replace = "cs",
-        update_n_lines = "sn",
+        update_n_lines = "msn",
       },
     },
   },
