1709cdf483bdd3cd1306dcb00853940d08f356fb
Matthew Winfield <Matthew_Winfield@Derivco.co.uk>
2024-04-18 09:06:19 +0100

    Show hidden files in Oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,15 +1,16 @@
 return {
   {
     "stevearc/oil.nvim",
-    opts = {},
+    opts = {
+      view_options = {
+        show_hidden = true
+      }
+    },
     -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" },
     lazy = false,
     keys = {
       { "<leader>e", "<cmd>Oil<cr>", desc = "Open file explorer (Oil)" },
     },
-    config = function()
-      require("oil").setup()
-    end,
   },
 }
