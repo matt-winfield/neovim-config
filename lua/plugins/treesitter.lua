cb536f83b9f98b7fa97c4a847bf2544f39b0601f
Matthew Winfield <Matthew_Winfield@Derivco.co.uk>
2024-05-28 08:59:01 +0100

    Disable treesitter highlight in HTML

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -13,6 +13,22 @@ return {
     opts = {
       highlight = {
         enable = true,
+        disable = function(lang, buf)
+          if lang == "html" then
+            -- Treesitter highlight crashes in HTML
+            return true
+          end
+          local max_filesize = 100 * 1024 -- 100 KB
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            vim.notify(
+              "File larger than 100KB treesitter disabled for performance",
+              vim.log.levels.WARN,
+              { title = "Treesitter" }
+            )
+            return true
+          end
+        end,
       },
       ensure_installed = {
         "html",
