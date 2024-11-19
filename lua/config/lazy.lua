62214ed5339ebd6b83fe722243c60631c8643adf
Matthew Winfield <Matthew_Winfield@Derivco.co.uk>
2024-11-19 13:16:16 +0000

    Fix eslint detection

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,7 +16,6 @@ require("lazy").setup({
     -- import/override with your plugins
     { import = "lazyvim.plugins.extras.util.project" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
-    { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "plugins" },
   },
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,9 +1,3 @@
 return {
   "stevearc/conform.nvim",
-  opts = {
-    format_on_save = {
-      -- Formatting can be quite slow on work machine, so give it a long timeout
-      timeout_ms = 10000,
-    },
-  },
 }
