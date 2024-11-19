cb3cbab54159f14d50702ea481130b1ad22d60ec
Matthew Winfield <Matthew_Winfield@Derivco.co.uk>
2024-11-19 15:10:11 +0000

    Increase format timeout

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,3 +1,9 @@
 return {
   "stevearc/conform.nvim",
+  opts = {
+    format = {
+      -- Formatting can be quite slow on work machine, so give it a long timeout
+      timeout_ms = 10000,
+    },
+  },
 }
