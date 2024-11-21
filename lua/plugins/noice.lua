6ae319e09e96bfe767ecb6921d2abd2d39c571ba
Matthew Winfield <Matthew_Winfield@Derivco.co.uk>
2024-11-21 12:17:21 +0000

    Re-enable noice (works better on Windows now)

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,6 +1,23 @@
 return {
   {
     "folke/noice.nvim",
-    enabled = false, -- It's pretty slow, and messes up on Windows when trying to do commands containing slashes
+    -- enabled = false, -- It's pretty slow, and messes up on Windows when trying to do commands containing slashes
+    opts = {
+      cmdline = {
+        enabled = false,
+      },
+      messages = {
+        enabled = false,
+      },
+      popupmenu = {
+        enabled = false,
+      },
+      redirect = {
+        enabled = false,
+      },
+      notify = {
+        enabled = false,
+      },
+    },
   },
 }
