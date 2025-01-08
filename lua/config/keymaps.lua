1336d67ee0537343fce0ca693b218cd9337c009d
Matthew Winfield <Matthew_Winfield@Derivco.co.uk>
2025-01-08 11:37:54 +0000

    Add neovim snacks

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -33,6 +33,13 @@ wk.register({
 -- Yank line excluding leading/trailing whitespace
 map("n", "<leader>yl", "^yg_", { desc = "Yank line contents to clipboard" })
 
+-- Notification group
+wk.register({
+  ["<leader>n"] = {
+    name = "+notifications",
+  },
+})
+
 -- Map keybinds to change indentation settings
 map("n", "<leader>i2", function()
   vim.bo.expandtab = true
