4fb330b9b5d20fe293eccdf1f6a822bbd0617d52
Matthew Winfield <Matthew_Winfield@Derivco.co.uk>
2025-02-11 10:27:04 +0000

    Set completion color for files

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -9,6 +9,7 @@ return {
 
       vim.api.nvim_set_hl(0, "CmpItemKindVariable", { fg = "#CA92FC" })
       vim.api.nvim_set_hl(0, "CmpItemKindInterface", { link = "CmpItemKindVariable" })
+      vim.api.nvim_set_hl(0, "CmpItemKindFile", { fg = "#59DBFC" })
     end,
   },
 }
