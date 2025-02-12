e3597d868f3ccd7dc3312826eb3e3d6eeee979fd
Matthew Winfield <Matthew_Winfield@Derivco.co.uk>
2025-02-12 11:10:58 +0000

    Default to not disabling prebuilt binaries download

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -12,11 +12,11 @@ return {
         end,
       },
     }
-    opts.fuzzy = {
-      prebuilt_binaries = {
-        download = false, -- SSL certificate fails to download, must install manually - See https://cmp.saghen.dev/configuration/fuzzy.html#installation
-      },
-    }
+    -- opts.fuzzy = {
+    --   prebuilt_binaries = {
+    --     download = false, -- SSL certificate fails to download, must install manually - See https://cmp.saghen.dev/configuration/fuzzy.html#installation
+    --   },
+    -- }
     vim.api.nvim_set_hl(0, "BlinkCmpKindVariable", { fg = "#CA92FC" })
     vim.api.nvim_set_hl(0, "BlinkCmpKindInterface", { link = "CmpItemKindVariable" })
     vim.api.nvim_set_hl(0, "BlinkCmpKindFile", { fg = "#59DBFC" })
