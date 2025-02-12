02efb0b4eb876cacf20248e5028b2aa6f009d370
Matthew Winfield <Matthew_Winfield@Derivco.co.uk>
2025-02-12 14:42:20 +0000

    Use prettierd formatting

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,9 +1,15 @@
 return {
   "stevearc/conform.nvim",
   opts = {
-    format = {
+    default_format_opts = {
       -- Formatting can be quite slow on work machine, so give it a long timeout
       timeout_ms = 10000,
+    },
+    formatters_by_ft = {
+      javascript = { "prettierd" },
+      javascriptreact = { "prettierd" },
+      typescript = { "prettierd" },
+      typescriptreact = { "prettierd" },
     },
   },
 }
