a1433cab3c4fe9a81a6b245e59cf0e911516e654
Matthew Winfield <Matthew_Winfield@Derivco.co.uk>
2025-02-19 14:45:22 +0000

    Show current directory in oil winbar

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -3,8 +3,11 @@ return {
     "stevearc/oil.nvim",
     opts = {
       view_options = {
-        show_hidden = true
-      }
+        show_hidden = true,
+      },
+      win_options = {
+        winbar = "%{v:lua.require('oil').get_current_dir()}",
+      },
     },
     -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" },
