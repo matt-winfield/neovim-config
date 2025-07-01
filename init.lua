92965ff133dd1a6ab728c9391cb17016eb8d7905
Matt Winfield <Matthew_Winfield@derivco.co.uk>
2025-07-01 14:23:50 +0100

    Fix undo keymaps being overwritten by lazyvim in vscode and undo-ing too much

--- init.lua
+++ init.lua
@@ -1,1 +1,11 @@
 require("config.lazy")
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "VeryLazy",
+  callback = function()
+    if vim.g.vscode then
+      vim.keymap.del("n", "u")
+      vim.keymap.del("n", "<C-r>")
+    end
+  end,
+})
